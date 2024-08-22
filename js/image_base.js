0cf8e47842661f2e361bfda66bfb89a7fb81d502
Revy <162366071+teoto4@users.noreply.github.com>
2024-08-22 22:49:38 +0300

    first commit

---
+++ js/image_base.js
@@ -0,0 +1,13 @@
+export const image_base = {
+  first_slide:
+    "https://i.pinimg.com/originals/9c/7b/e4/9c7be43979a736a8695361a544630b97.jpg",
+  second_slide:
+    "https://wallpaper-house.com/data/out/10/wallpaper2you_393472.jpg",
+  third_slide:
+    "https://i.pinimg.com/originals/8e/51/30/8e51302d51089d0f234dc16314c4e5b6.jpg",
+
+  fourth_slide:
+    "https://wallpapers.com/images/hd/1920x1080-aesthetic-glrfk0ntspz3tvxg.jpg",
+  fifth_slide:
+    "https://cdn.pixabay.com/photo/2016/10/21/14/50/plouzane-1758197_1280.jpg",
+};
---
+++ main.js
@@ -0,0 +1,70 @@
+import { image_base } from "./js/image_base.js";
+
+console.log(image_base.first_slide);
+
+const slide = document.querySelector(".slide");
+const arrow_left = document.querySelector(".arrow_png");
+const arrow_right = document.querySelector(".arrow_png_right");
+const timer = document.querySelector(".timer");
+
+console.dir(slide);
+
+function addSlide(target_slide) {
+  slide.src = target_slide;
+}
+
+addSlide(image_base.first_slide);
+
+let slide_index = 0;
+
+const slider_array = [];
+for (let key in image_base) {
+  slider_array.push(key);
+}
+
+console.log(slider_array[1]);
+console.log(image_base[slider_array[0]]);
+
+arrow_left.addEventListener("click", (e) => {
+  slide.classList.add("slide_back");
+  slide.classList.replace("slide_back", "slide_effect");
+  setTimeout(backSlide, 500);
+});
+arrow_right.addEventListener("click", (e) => {
+  slide.classList.add("slide_back");
+  slide.classList.replace("slide_back", "slide_effect");
+  setTimeout(nextSlide, 500);
+});
+
+function nextSlide() {
+  slide_index += 1;
+  if (slide_index > 4) {
+    slide_index = 0;
+  }
+  addSlide(image_base[slider_array[slide_index]]);
+  console.log(slide_index);
+  slide.classList.replace("slide_effect", "slide_back");
+  console.log(slide_index);
+}
+
+function backSlide() {
+  slide_index -= 1;
+  console.log(slide_index);
+  if (slide_index <= 0) {
+    slide_index = 4;
+  }
+  addSlide(image_base[slider_array[slide_index]]);
+  console.log(slide_index);
+  slide.classList.replace("slide_effect", "slide_back");
+  console.log(slide_index);
+}
+
+timer.addEventListener("click", (e) => {
+  setInterval(nextSlideTime, 3700);
+});
+
+function nextSlideTime() {
+  slide.classList.add("slide_back");
+  slide.classList.replace("slide_back", "slide_effect");
+  setTimeout(nextSlide, 700);
+}
